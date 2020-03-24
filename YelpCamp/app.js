8420f0fe56b4f4661b700f4ea1885cd136f0b069
cgupta3131 <cgupta3131@gmail.com>
2020-03-24 16:40:30 +0530

    RESTful Blog App Done

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,37 +1,101 @@
 var express = require('express');
 var app = express();
-var bodyPareser = require('body-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-app.use(bodyPareser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});	
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.render("landing");
 });
 
-var campgrounds = [
-    {name:"Chirag's Camp",image:"https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c7d2b7ddd9249cc5f_340.jpg"},
-    {name:"Disha's Camp",image:"https://pixabay.com/get/57e8d34b4c50a814f6da8c7dda793f7f1636dfe2564c704c7d2b7ddd9249cc5f_340.jpg"},
-    {name:"Lavish's Camp",image:"https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2b7ddd9249cc5f_340.jpg"}
-]
+//SCHEME SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
 
-app.get("/campgrounds",function(req,res){
-    
-    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+/*Campground.create({
+    name:"Chirag's Camp",
+    image:"https://combo.staticflickr.com/ap/build/images/jobs/good-panda.jpg",
+    description: "This is Chirag's Camp"
+}, function(err,campground){
+if(err){
+        console.log("Something went wrong");
+    }
+    else{
+        console.log("Campground added to the database");
+        console.log(campground);
+    }
+});*/
+
+var campgrounds = [
+    {name:"Chirag's Camp", image:"https://c1.staticflickr.com/5/4471/23727254668_bb82678d67_z.jpg"},
+    {name:"Disha's Camp", image:"https://combo.staticflickr.com/ap/build/images/jobs/good-panda.jpg"},
+    {name:"Lavish's Camp", image:"https://live.staticflickr.com/65535/48441271856_af538f5568_b.jpg"}
+]
+
+//INDEX - show all campgrounds
+app.get("/campgrounds",function(req,res){
+    //Get all the campgrounds from database
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log("Some Error occured");
+            console.log(err);
+        }
+        else{
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    })
+});
+
+//CREATe - add new campground to DB
 app.post("/campgrounds", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
+    var desc = req.body.description;
 
-    var newCampground = {name:name, image:image};
-    campgrounds.push(newCampground);
+    var newCampground = {name:name, image:image, description: desc};
+    //Create a new campground and add to dataBase
+    Campground.create(newCampground, function(err,campground){
+    if(err){
+            console.log("Something went wrong");
+        }
+        else{
+            console.log("Campground added to the database");
+            console.log(campground);
+        res.redirect("/campgrounds");
+        }
+    });
 
-    res.redirect("/campgrounds");
 });
 
+//NEW - show form to create new campground
 app.get("/campgrounds/new",function(req,res){
     res.render("new");
+});
+
+//SHOw - displays information about one campground
+app.get("/campgrounds/:id",function(req,res){
+
+    Campground.findById(req.params.id, function(err,foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("foundCampground: " + foundCampground);
+            //res.render("index", {campgrounds: foundCampground});
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 
